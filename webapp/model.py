24c3c93e51b9dcdf9832c20d19d96d6a22536325
KaterusiaFX <rusik888@gmail.com>
2020-05-20 18:18:53 +0300

    added user login, admin page

--- webapp/model.py
+++ webapp/model.py
@@ -1,3 +1,5 @@
+from flask_login import UserMixin  # provides is_authenticated, is_active, is_anonymous, get_id methods
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -12,3 +14,25 @@ class News(db.Model):
 
     def __repr__(self):  # python magic method, defines how to print News object from db
         return '<News {} {}>'.format(self.title, self.url)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(57), index=True, unique=True)
+    password = db.Column(db.String(130))
+    role = db.Column(db.String(12), index=True)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @property  # this decorator allows to call method as an attribute, without brackets ()
+    def is_admin(self):
+        return self.role == 'admin'
+
+    def __repr__(self):
+        return '<User {} {}>'.format(self.username, self.role)
+
+
